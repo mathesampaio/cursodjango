72bf2191965481a063fecbffae2a27aef16dddf0
matheus.sampaio <matheussampaiomedeiros@gmail.com>
2020-04-19 19:52:40 -0300

    agora vai

--- django1/settings.py
+++ django1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '34p8_!vht6-jk3s+hv75*)w%*$q!+f5%uhtadn#a5&vhn$d5s9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
